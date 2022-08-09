53a01639e997d19af70648c6bbe60af2888a23ea
take-mac <043546shutta+mac@mediceo-gp.com>
2022-08-09 17:06:45 +0900

    2022.08.09

--- TelSearch2/ViewController.swift
+++ TelSearch2/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // 初期画面生成
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 認証チェック
     }
 
+    // 検索処理
+    
+    
+    
+    // 電話
 
+    
+    
+    
+    
 }
 
