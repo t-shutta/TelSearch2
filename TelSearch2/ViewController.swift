3a04629dd1ab2d931b6eb49601445fabc02fdf59
take-mac <043546shutta+mac@mediceo-gp.com>
2022-08-09 11:50:58 +0900

    Initial Commit

---
+++ TelSearch2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelSearch2
+//
+//  Created by medipad-no-MacBook-Pro on 2022/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
